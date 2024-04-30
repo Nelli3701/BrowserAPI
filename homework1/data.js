0c26e39fc73374c809c308151da73fb4a6623df3
Nelli3701 <89271519+Nelli3701@users.noreply.github.com>
2024-04-30 15:51:47 +0500

    Add files via upload

---
+++ homework1/data.js
@@ -0,0 +1,16 @@
+const classes = `[
+    {
+        "title": "zumba",
+        "img": "img/zumba.webp",
+        "time": "10:00",
+        "maxMembers": 10,
+        "currentMembers": 10
+    },
+    {
+        "title": "choreography",
+        "img": "img/choreo.webp",
+        "time": "11:00",
+        "maxMembers": 12,
+        "currentMembers": 11
+    }
+]`;
---
+++ homework1/script.js
@@ -0,0 +1,55 @@
+'use strict';
+const parseData = JSON.parse(classes);
+const containerEl = document.querySelector('.container');
+const template = item__template.content;
+parseData.forEach((el) => {
+    const newTemplate = template.cloneNode(true);
+    newTemplate.querySelector('img').src = el.img;
+    newTemplate.querySelector('img').alt = el.title;
+    newTemplate.querySelector('h5').textContent = el.title;
+    newTemplate.querySelector('.time').textContent = el.time;
+    newTemplate.querySelector('.max-members b').textContent = el.maxMembers;
+    newTemplate.querySelector('.current-members b').textContent = el.currentMembers;
+    containerEl.appendChild(newTemplate);
+});
+const savedClasses = JSON.parse(localStorage.getItem('classes')) || [];
+const saveData = () => {
+    localStorage.setItem('classes', JSON.stringify(savedClasses));
+}
+
+const btnSuccessEls = document.querySelectorAll('.btn-success');
+btnSuccessEls.forEach((button) => {
+    const parentEl = button.parentElement;
+    const maxMembers = parentEl.querySelector('.max-members b');
+    const currentMembers = parentEl.querySelector('.current-members b');
+    if (Number(currentMembers.textContent) == Number(maxMembers.textContent)) {
+        button.textContent = 'Нет мест';
+        button.disabled = true;
+    }
+});
+
+document.querySelectorAll('.btn-success').forEach((button) => button.addEventListener('click', (e) => {
+    const parentEl = button.parentElement;
+    const maxMembers = parentEl.querySelector('.max-members b');
+    const currentMembers = parentEl.querySelector('.current-members b');
+    if (Number(currentMembers.textContent) < Number(maxMembers.textContent)) {
+        currentMembers.textContent = Number(currentMembers.textContent) + 1;
+        button.textContent = 'Вы записаны';
+        button.disabled = true;
+        parentEl.querySelector('.btn-danger').style.display = 'block';
+        parentEl.querySelector('.btn-danger').disabled = false;
+        parentEl.querySelector('.btn-danger').textContent = 'Отмена записи';
+        saveData();
+    }
+}));
+
+document.querySelectorAll('.btn-danger').forEach((button) => button.addEventListener('click', (e) => {
+    const parentEl = button.parentElement;
+    const currentMembers = parentEl.querySelector('.current-members b');
+    currentMembers.textContent = Number(currentMembers.textContent) - 1;
+    button.textContent = 'Отмена записи';
+    button.disabled = true;
+    parentEl.querySelector('.btn-success').disabled = false;
+    parentEl.querySelector('.btn-success').textContent = 'Записаться';
+    saveData();
+}));
